18d443f1004a9b2cae19746c5b9cfd1576bcc5d6
Feranic <feranic@yandex.ru>
2020-01-28 13:38:40 +0300

    Ваше жилье, в процессе

--- Fixtures/Locators/GoodChoiceLocators.py
+++ Fixtures/Locators/GoodChoiceLocators.py
@@ -5,3 +5,11 @@ class GoodChoiceLocators:
     Button1 = (By.CSS_SELECTOR, "div.mt-3 tr:last-child>td:nth-child(2)>button")
     Button_Flat = (By.ID, "content.object.data.type-flat")
     Region_Input = (By.ID,"react-select-2-input")
+    City = (By.ID, "content-object-data-address-registration-city-name")
+    Street = (By.ID, "content-object-data-address-registration-street-name")
+    House = (By.ID,"content-object-data-address-registration-house")
+    Corp = (By.ID, "content-object-data-address-registration-corp")
+    Building = (By.ID, "content-object-data-address-registration-building")
+    Flat = (By.ID, "content-object-data-address-registration-flat")
+    Continue_Button1 = (By.CSS_SELECTOR, "div.text-right>button")
+    
