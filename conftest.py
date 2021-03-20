3a1313b9c0dd6eacde0ebadb4ed71f59752251df
Feranic <feranic@yandex.ru>
2021-03-20 15:25:29 +0300

    Сделал оперу брауером по умолчанию для webdriver

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 def pytest_addoption(parser):
     parser.addoption("--days", action="store", default=6, type=int)
-    parser.addoption("--browser", action="store", default="chrome", type=str)
+    parser.addoption("--browser", action="store", default="opera", type=str)
     parser.addoption("--target", action="store", default="1", type=str)
     parser.addoption("--product", action="store", default="Flatbase", type=str)
