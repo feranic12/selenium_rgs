c6e54b80fd91572947df04329fdb08fbf4a7ebc9
Kirill Nikitin <feranic@yandex.ru>
2021-06-06 13:14:22 +0300

    Изменил структуру проекта, кажется, ничего не сломалось.

---
+++ src/config.py
@@ -0,0 +1,6 @@
+chromedriver_path = r"D:\KIRILL\Utilities\chromedriver.exe"
+chrome_binary_path = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+geckodriver_path = r"D:\KIRILL\Utilities\geckodriver.exe"
+firefox_binary_path = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+iedriver_path = r"D:\KIRILL\Utilities\IEDriverServer.exe"
+operadriver_path = r"D:\KIRILL\Utilities\operadriver.exe"
