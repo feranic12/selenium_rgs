75bc0a4c532580f13e191b8ff06ed204e67a4dbe
Feranic <feranic@yandex.ru>
2021-07-21 13:10:42 +0300

    Finished TelemedMyHealth, not it works

--- src/Fixtures/TelemedMyHealthFixture.py
+++ src/Fixtures/TelemedMyHealthFixture.py
@@ -34,6 +34,7 @@ class TelemedMyHealthFixture(BaseFixture):
         driver.find_element(*TelemedMyHealthLocators.InsurerLastName).send_keys(u"Петров")
         driver.find_element(*TelemedMyHealthLocators.InsurerFirstName).send_keys(u"Петр")
         driver.find_element(*TelemedMyHealthLocators.InsurerMiddleName).send_keys(u"Петрович")
+        driver.find_element(*TelemedMyHealthLocators.InsurerDob).send_keys("01011990")
         driver.find_element(*TelemedMyHealthLocators.InsurerMale).click()
         driver.find_element(*TelemedMyHealthLocators.InsurerPhone).send_keys("+7(123)123-21-32")
         driver.find_element(*TelemedMyHealthLocators.InsurerEMail).send_keys("knikitin@avinfors.ru")
@@ -44,11 +45,11 @@ class TelemedMyHealthFixture(BaseFixture):
 
     def insured_info(self):
         driver = self.driver
-        driver.find_element_by_xpath("//div[@id='RI-product-steps']/div[3]/div/div[2]/button").click()
+        driver.find_element(*TelemedMyHealthLocators.ContinueButton2.click()
 
     def agree(self):
         driver = self.driver
-        driver.find_element_by_xpath("//div[@id='RI-product-steps']/div[4]/div/div/div/div/div/label").click()
+        driver.find_element(*TelemedMyHealthLocators.Atree).click()
 
     def fill_frame(self):
         driver = self.driver
