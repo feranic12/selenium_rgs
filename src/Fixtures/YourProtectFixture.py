c1cad6c09dd4447aa3355a6ffed7d89be6d67726
Feranic <feranic@yandex.ru>
2021-10-10 19:38:27 +0300

    Done filler for YourProtect

--- src/Fixtures/YourProtectFixture.py
+++ src/Fixtures/YourProtectFixture.py
@@ -27,17 +27,39 @@ class YourProtectFixture(BaseFixture):
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(YourProtectLocators.Sport_Yes))
         driver.find_element(*YourProtectLocators.Sport_Yes).click()
         driver.find_element(*YourProtectLocators.Term_Months_3).click()
-        driver.find_element(*YourProtectLocators.Continue_Button_1).click()
-    
+        driver.find_element(*YourProtectLocators.Continue_Button).click()
+
     def insured_info(self):
         driver = self.driver
         WebDriverWait(driver, 10).until(EC.presence_of_element_located(YourProtectLocators.Sports_Checkbox))
         driver.find_element(*YourProtectLocators.Sports_Checkbox).click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "content.insConditions.insVariant.sportList.data")))
+        driver.find_element_by_id("content.insConditions.insVariant.sportList.data").send_keys("Теннис(настольный)" + Keys.ENTER)
         driver.find_element(By.NAME, "content.insuredPerson.list[0].lastName").send_keys("Петров")
         driver.find_element(By.NAME, "content.insuredPerson.list[0].firstName").send_keys("Пётр")
         driver.find_element(By.NAME, "content.insuredPerson.list[0].middleName").send_keys("Петрович")
         driver.find_element(By.NAME, "content.insuredPerson.list[0].dob").send_keys("01011990")
+        driver.find_element(By.NAME, "content.insuredPerson.list[0].phone").send_keys("1231231212")
+        driver.find_element(*YourProtectLocators.Male).click()
+        driver.find_element(*YourProtectLocators.IsInsurer).click()
+        driver.find_element(*YourProtectLocators.DMS).click()
+        driver.find_element(*YourProtectLocators.Mite).click()
+        driver.find_element(*YourProtectLocators.Continue_Button).click()
+
+    def insurer_info(self):
+        driver = self.driver
+        WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.NAME, "content.policyHolder.email")))
+        driver.find_element(By.NAME, "content.policyHolder.email").send_keys("knikitin@avinfors.ru")
+        driver.find_element(By.NAME, "content.policyHolder.email2").send_keys("knikitin@avinfors.ru")
+        driver.find_element(*YourProtectLocators.Continue_Button).click()
+
+    def agree(self):
+        driver = self.driver
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(YourProtectLocators.Accept))
+        driver.find_element(*YourProtectLocators.Accept).click()
 
     def fill_frame(self):
         self.conditions()
         self.insured_info()
+        self.insurer_info()
+        self.agree()
--- src/Locators/YourProtectLocators.py
+++ src/Locators/YourProtectLocators.py
@@ -4,5 +4,10 @@ from selenium.webdriver.common.by import By
 class YourProtectLocators:
     Sport_Yes = (By.CSS_SELECTOR, "div[data-name=\"content.insConditions.insVariant.sport\"] button:first-child ")
     Term_Months_3 = (By.CSS_SELECTOR, "div[data-name=\"content.insConditions.insVariant.termMonths\"] button:nth-child(2)")
-    Continue_Button_1= (By.CSS_SELECTOR, "div.ml-auto > button")
+    Continue_Button= (By.CSS_SELECTOR, "div.ml-auto > button")
     Sports_Checkbox = (By.CSS_SELECTOR, "label[for=\"content.insConditions.insVariant.sportListSame_custom_input\"]")
+    Male = (By.CSS_SELECTOR, "div[data-name=\"content.insuredPerson.list[0].sex\"] button:first-child")
+    IsInsurer = (By.CSS_SELECTOR, "label[for=\"content.insuredPerson.list[0].data.policyHolder_custom_input\"]")
+    DMS = (By.CSS_SELECTOR, "label[for=\"content.insConditions.insVariant.additionals.medical_custom_input\"]")
+    Mite = (By.CSS_SELECTOR, "label[for=\"content.insConditions.insVariant.additionals.mite_custom_input\"]")
+    Accept = (By.CSS_SELECTOR, "label[for=\"content.temp.accept_custom_input\"]")
