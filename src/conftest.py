93da9ec2064f41b67da9dd03734fd2416b00501c
Feranic <feranic@yandex.ru>
2021-12-08 20:18:51 +0300

    Created new conftest file in project's root directory containg hook function pytest_addoption

---
+++ src/conftest.py
@@ -0,0 +1,6 @@
+def pytest_addoption(parser):
+    parser.addoption("--days", action="store", default=6, type=int)
+    parser.addoption("--browser", action="store", default="chrome", type=str)
+    parser.addoption("--target", action="store", default="1", type=str)
+    parser.addoption("--product", action="store", default="Voyage", type=str)
+    parser.addoption("--lang", action="store", default="ENG", type=str)
